a134563ea4723c0152686367aad7746e6c268974
cristianmusica <75284701+cristianmusica@users.noreply.github.com>
2023-10-31 18:11:16 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,103 @@
+import discord
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Bot is ready.')
+
+    # Change current directory to root
+    os.chdir('/')
+
+    # Change current directory to "/var/www/html/"
+    os.chdir('/var/www/html/')
+
+@bot.command()
+async def modify_index(ctx):
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+
+    await ctx.send("Presiona el botón 'Modificar' para continuar.")
+    
+    def check_button(m):
+        return m.author == ctx.author and m.content.lower() == "modificar" and m.channel == ctx.channel
+
+    try:
+        await bot.wait_for('message', check=check_button, timeout=60)
+        await ctx.send("Adelante, dime el nombre del archivo a modificar.")
+
+        try:
+            filename = await bot.wait_for('message', check=check, timeout=60)
+            filename = filename.content.strip()
+
+            if os.path.isfile(filename):
+                await ctx.send("Genial! El archivo está disponible. Envía el código para modificarlo.")
+
+                try:
+                    code = await bot.wait_for('message', check=check, timeout=60)
+                    code = code.content.strip()
+
+                    with open(filename, 'w') as file:
+                        file.write(code)
+
+                    await ctx.send(f"El archivo '{filename}' ha sido modificado exitosamente.")
+                except asyncio.TimeoutError:
+                    await ctx.send("No se ha proporcionado el código a tiempo.")
+            else:
+                await ctx.send("Oh no, al parecer ese archivo no está disponible en la carpeta actual.")
+        except asyncio.TimeoutError:
+            await ctx.send("No se ha proporcionado el nombre del archivo a tiempo.")
+    except asyncio.TimeoutError:
+        await ctx.send("No se ha presionado el botón 'Modificar' a tiempo.")
+
+@bot.command()
+async def create_file(ctx):
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+
+    await ctx.send("Presiona el botón 'Crear' para continuar.")
+    
+    def check_button(m):
+        return m.author == ctx.author and m.content.lower() == "crear" and m.channel == ctx.channel
+
+    try:
+        await bot.wait_for('message', check=check_button, timeout=60)
+        await ctx.send("¿Dónde deseas crear el archivo?")
+
+        try:
+            location = await bot.wait_for('message', check=check, timeout=60)
+            location = location.content.strip()
+
+            if location.lower() == "aca":
+                await ctx.send("Adelante, dime el nombre del archivo.")
+
+                try:
+                    filename = await bot.wait_for('message', check=check, timeout=60)
+                    filename = filename.content.strip()
+
+                    with open(filename, 'w') as file:
+                        file.write("")
+
+                    await ctx.send(f"El archivo '{filename}' ha sido creado exitosamente en la ubicación actual.")
+                except asyncio.TimeoutError:
+                    await ctx.send("No se ha proporcionado el nombre del archivo a tiempo.")
+            else:
+                await ctx.send("Ubicación no reconocida.")
+        except asyncio.TimeoutError:
+            await ctx.send("No se ha proporcionado la ubicación a tiempo.")
+    except asyncio.TimeoutError:
+        await ctx.send("No se ha presionado el botón 'Crear' a tiempo.")
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="Bot Commands", description="List of available commands:", color=discord.Color.blue())
+    embed.add_field(name="!modify_index", value="Modifies the specified file in the current directory.", inline=False)
+    embed.add_field(name="!create_file", value="Creates a new file in the specified location.", inline=False)
+    embed.add_field(name="!help", value="Displays this help message.", inline=False)
+    embed.add_field(name="Modificar", value="Press this button to modify a file.", inline=False)
+    embed.add_field(name="Crear", value="Press this button to create a file.", inline=False)
+    await ctx.send(embed=embed)
+
+bot.run('YOUR_DISCORD_TOKEN')
